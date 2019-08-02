7ff68a416a4c364654debd83e7b9e5df1224f31d
Drew Banin <drew@fishtownanalytics.com>
2019-08-02 11:10:46 -0400

    Sync streams in the correct order (Contacts before CallLogs) (#2)

--- tap_ringcentral/streams/__init__.py
+++ tap_ringcentral/streams/__init__.py
@@ -4,26 +4,13 @@ from tap_ringcentral.streams.call_log import CallLogStream
 from tap_ringcentral.streams.company_call_log import CompanyCallLogStream
 from tap_ringcentral.streams.messages import MessageStream
 
-# This is disabled because RingCentral's permission model
-# does not permit a user to fetch Meetings for other
-# RingCentral users. It should be re-enabled RingCentral makes
-# these resources accessible in its API
-#
-#from tap_ringcentral.streams.meetings import MeetingStream
-
 
 AVAILABLE_STREAMS = [
     ContactsStream,
     CallLogStream,
     CompanyCallLogStream,
     MessageStream,
-    #MeetingStream,
 ]
 
-__all__ = [
-    'ContactsStream',
-    'CallLogStream',
-    'CompanyCallLogStream',
-    'MessageStream',
-    #'MeetingStream',
-]
+
+__all__ = [s.NAME for s in AVAILABLE_STREAMS]
